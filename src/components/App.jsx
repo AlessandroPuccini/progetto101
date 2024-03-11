c5d856724fb048b71b23fc3ef0def5172e2472d7
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-03-11 23:14:22 +0100

    creazione app.js e primo css

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import IMAGES from '../images/Images';
+import { Link } from 'react-router-dom';
+
+function App() {
+
+
+  return (
+    <>
+     <div id='homePage'>
+          <Link to='/man'>
+          <img src={IMAGES.image1} alt='immagine uomo' />
+          </Link>
+       <Link to='/woman'>
+          <img src={IMAGES.image2} alt='immagine donna' />
+          </Link>
+      </div>
+   
+    </>
+  );
+}
+export default App;
