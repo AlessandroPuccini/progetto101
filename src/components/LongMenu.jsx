736530d7cc6479d1746b8dffbe7c6d1d9e41044b
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-03-11 23:58:03 +0100

    creazione LongMenu

---
+++ src/components/LongMenu.jsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+
+const options = [
+  'MAN',
+  'WOMAN',
+  'CATALOG',
+  'ABOUT',
+  'CONTACTS',
+];
+const ITEM_HEIGHT = 68;
+
+export default function LongMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+ 
+  return (
+    <div>
+      <DensityMediumIcon
+        fontSize='large'
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        
+      </DensityMediumIcon>
+      <Menu
+        id="long-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '15ch',
+          },
+        }}
+      >
+        {options.map((option) => (
+          <MenuItem key={option}  >
+            <Link to={`/${option}`}>{option}</Link>
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
