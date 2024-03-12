f8b1489bb4f2c3381f3be2154d61924173e0350e
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-03-12 11:03:43 +0100

    aggiunta pagina contatti

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Button } from '@mui/material'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
+
 
 function Contacts() {
+
+const handleClick = () => {
+  alert('Thank you!, We will answer to your email as quick as possible');
+}
+
   return (
-    <div>
-      <h1>CONTACTS</h1>
+    <>
+    <Navbar />
+    <div className='contact-paragraph'>
+      <h3>QUESTIONS? WE ARE HERE TO HELP YOU</h3>
+      <p>We will answer to every email within 24 hours Monday to Friday, excluding holidays.
+        <br></br>
+        If you prefer, you can call us on +39 0732 346739 or +39 343 4590897: we are at your disposal from Monday to Friday, from 9.00 to 18.00 CET, excluding holidays. </p>
     </div>
+    <div className='contact-form'>
+       <input type='text' placeholder='Name' />
+       <input type='text' placeholder='Surname' />
+       <input type='text' placeholder='Numero ordine' />
+       <input type='text' placeholder='Email' />
+        <textarea placeholder='Message' />
+        <button onClick={handleClick}>Send</button>
+    </div>
+    <Footer />
+    </>
   )
 }
 
