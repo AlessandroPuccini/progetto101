72cae61197a717cab35f9f3f295a7a3c0a84f73c
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-03-14 13:27:34 +0100

    aggiunta coding to write on datapbase sul file input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from 'axios';
 
-function Input({id, info, reg, error, errorMsg, type='text', rules={} }) {
+function Input({id, info, reg, error, errorMsg, type='string', rules={} }) {
+
+let [inputValue, setInputValue] = useState('');
+
+const saveData = async () => {
+    try{
+      await axios.post('http://localhost:3001/writetodatabase', {content: inputValue});
+        console.log("Data:",inputValue );
+        alert("Data saved:", inputValue);
+        
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+
   return (
     <div className='admin-login'>
         <label htmlFor={id}> {info}</label>
       <input 
       {...reg(info, rules)}
+      value={inputValue}
+      onChange={e => setInputValue(e.target.value)}
       type={type}
       id={id}
       placeholder={info}
