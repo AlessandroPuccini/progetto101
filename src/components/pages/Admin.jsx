1c5d3ab85bd5654a552b1ddab95445c35da1a5eb
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-03-14 13:47:09 +0100

    modifiche al file Input e Admin e modifica estensione file Read.jsx

--- src/components/pages/Admin.jsx
+++ src/components/pages/Admin.jsx
@@ -1,10 +1,12 @@
-import React, {useId} from 'react';
+import React, {useId, useState} from 'react';
+import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 import Divider from '@mui/material/Divider';
 import '../../style/index.css';
 import Input from '../Input';
+import Read from '../Read';
 
 
 function Admin(errorMsg) {
@@ -20,6 +22,20 @@ function Admin(errorMsg) {
       alert('Form submitted'); 
     };
 
+    let [inputValue, setInputValue] = useState('');
+
+    const saveData = async () => {
+        try{
+          await axios.post('http://localhost:3001/writetodatabase', {content: inputValue});
+            console.log("Data:",inputValue );
+            alert("Data saved:", inputValue);
+    
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+
+
   return (
     
      <>
@@ -29,6 +45,8 @@ function Admin(errorMsg) {
         <h3> ALREADY HAVE AN ACCOUNT? SIGN IN</h3>
         </div>
      <Input id={idPrefix + '-email'}
+     value={inputValue}
+     onChange={e => setInputValue(e.target.value)}
      rules={{required: true, pattern: /^\S+@\S+$/i}}
      info='Email'
      reg={register}
@@ -36,6 +54,8 @@ function Admin(errorMsg) {
      errorMsg="Use a valid email address"
      />
      <Input id={idPrefix + '-password'}
+     value={inputValue}
+     onChange={e => setInputValue(e.target.value)}
      rules={{required: true, minLength: 8, maxLength: 20}}
      info='Password'
      type='password'
@@ -51,6 +71,8 @@ function Admin(errorMsg) {
     <h3>OR SIMPLY SIGNUP HERE</h3>
     </div>
     <Input id={idPrefix + '-name'}
+       value={inputValue}
+       onChange={e => setInputValue(e.target.value)}
     rules={{required: true}} 
     info='Name'
     reg={register}
@@ -58,6 +80,8 @@ function Admin(errorMsg) {
     errorMsg='Please enter your name'/>
 
     <Input id={idPrefix + '-surname'} 
+       value={inputValue}
+       onChange={e => setInputValue(e.target.value)}
     rules={{required: true}}
     info='Surname'
     reg={register}
@@ -65,18 +89,24 @@ function Admin(errorMsg) {
     errorMsg='Please enter your last name'/>
 
 <Input id={idPrefix + '-address'} 
+   value={inputValue}
+   onChange={e => setInputValue(e.target.value)}
     rules={{required: true}}
     info='Address'
     reg={register}
     error={errors.Address} />
 
 <Input id={idPrefix + '-city'} 
+   value={inputValue}
+   onChange={e => setInputValue(e.target.value)}
     rules={{required: true}}
     info='City'
     reg={register}
     error={errors.City} />
 
 <Input id={idPrefix + '-zip'}
+   value={inputValue}
+   onChange={e => setInputValue(e.target.value)}
     rules={{required: true, pattern: /^\d{5}$/}}
     info='Zip code'
     reg={register}
@@ -84,6 +114,8 @@ function Admin(errorMsg) {
     errorMsg='Please enter a valid zip code' />
 
 <Input id={idPrefix + '-email'}
+   value={inputValue}
+   onChange={e => setInputValue(e.target.value)}
      rules={{required: true, pattern: /^\S+@\S+$/i}}
      info='Email'
      reg={register}
@@ -91,6 +123,8 @@ function Admin(errorMsg) {
      errorMsg='Use a valid email address' 
      />
   <Input id={idPrefix + '-password'}
+     value={inputValue}
+     onChange={e => setInputValue(e.target.value)}
      rules={{required: true, minLength: 8, maxLength: 20}}
      info='Password'
      type='password'
@@ -99,6 +133,8 @@ function Admin(errorMsg) {
      errorMsg='Your password should be at least 8 characters long' 
      />
   <Input id={idPrefix + '-confirmPassword'}
+     value={inputValue}
+     onChange={e => setInputValue(e.target.value)}
      rules={{required: true, minLength: 8, maxLength: 20}}
      info='PasswordCheck'
      type='password'
@@ -108,7 +144,7 @@ function Admin(errorMsg) {
      />
 
     <div className='admin-button'>
-     <button type='submit'>Sign up</button>
+     <button onClick={saveData} type='submit'>Sign up</button>
     </div>
     </form>
     <Footer />
