f678e9e06e6d639c8bd74d460934cc26807e54e9
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-03-18 13:01:09 +0100

    aggiunta const PORT al file index.js su server

--- server/Index.js
+++ server/Index.js
@@ -14,5 +14,7 @@ app.post("/", async (req, res) => {
     res.send(result);
 });
 
-
-app.listen(4000);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
