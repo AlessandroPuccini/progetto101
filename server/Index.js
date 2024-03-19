0c8b02b98851095666d19b597222dc397a716a61
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-03-19 18:41:41 +0100

    modifiche ai file index.js e connessione al database

--- server/Index.js
+++ server/Index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 
-require('./db/connection');
+let {connect } = require('./db/connection');
 const Users = require('./Models/User');
 
 app.post("/", async (req, res) => {
@@ -14,7 +14,11 @@ app.post("/", async (req, res) => {
     res.send(result);
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+    await connect();
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+startServer();
--- server/db/connection.js
+++ server/db/connection.js
@@ -1,2 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb:// 127.0.0.1:27017/ecommerce');
+
+ async function connect() {
+    let connection = await mongoose.connect("mongodb+srv://puccinialessandro:123456--@ecommerce.zyruqns.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce");
+    console.log(connection);
+}
+module.exports.connect = connect;
