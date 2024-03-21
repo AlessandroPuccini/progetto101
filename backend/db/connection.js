6183210154e36bb18abaf8c125ca34757f8c3af5
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-03-22 00:28:19 +0100

    Crazione percorso backend

---
+++ backend/db/connection.js
@@ -0,0 +1,23 @@
+import { config } from 'dotenv';
+config({ path: 'config.env' });
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+},
+});
+
+try{
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You are good to go!");
+} catch(err) {
+    console.error(err);
+}
+let db = client.db("lovers");
+
+export default db;
---
+++ backend/routes/record.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import db from '../db/connection.js';
+import { ObjectId } from 'mongodb';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    let collection = await db.collection("records");
+    let results = await collection.find({}).toArray();
+    res.send(results).status(200);
+});
+
+router.get('/:id', async (req, res) => {
+    let collection = await db.collection("records");
+    let query = { _id: ObjectId(req.params.id) };
+    let result = await collection.findOne(query);
+
+    if(!result) res.send("Record not found").status(404);
+    else res.send(result).status(200);
+});
+
+router.post('/', async (req, res) => {
+    try{
+        let newDocument = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        };
+        let collection = await db.collection("records");
+        let result = await collection.insertOne(newDocument);
+        res.send(result).status(204);
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).send("Error adding record");
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const query = { _id: new ObjectId(req.params.id) };
+
+        const collection = db.collection("records");
+        let result = await collection.deleteOne(query);
+        res.send(result).status(200);
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error deleting record");
+    }
+});
+
+export default router;
