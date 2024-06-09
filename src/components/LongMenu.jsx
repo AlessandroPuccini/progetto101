510aff0c68c395f2ba2c392564b0934d5fc14c19
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-06-09 23:25:15 +0200

    modifiche pulsante roads

--- src/components/LongMenu.jsx
+++ src/components/LongMenu.jsx
@@ -11,16 +11,22 @@ const options = [
   'CONTACTS',
 ];
 
+
 export default function DropdownMenu() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+
   return (
-    <div className="dropdown-menu">
-      <button onClick={() => setIsOpen(!isOpen)}>
-      Roads
+    <div className="dropdown-menu" onClick={handleToggle}>
+      <button>
+      Explore
       </button>
       {isOpen && (
-        <div className="dropdown-menu-content">
+        <div className="dropdown-menu-content open">
           {options.map((option) => (
 
             <Link to={`/progetto101/${option}`} key={option}>{option} </Link>
